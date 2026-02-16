35da3b9c258b960345f4421269b9b83765306ca4
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-16 15:00:08 +0500

    kjkj

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,7 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
-import productRouter from "./routes/productRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
-import sliderRouter from "./routes/sliderRoute.js";
+
 
 
 // App Config
@@ -25,8 +22,7 @@ app.use(express.json());
 
 // CORS Configuration (Specific Domain Allow Karo)
 const allowedOrigins = [
-  'http://localhost:5173', 
-  'https://jamalcollection.com', 
+  'http://localhost:5173',  
   'https://ecommerce-store-mern-lake.vercel.app'
 ];
 
@@ -44,11 +40,7 @@ app.use(cors({
 
 // Api Endpoints
 app.use("/api/user", userRouter);
-app.use("/api/product", productRouter);
-app.use("/api/cart", cartRouter);
 
-app.use("/api/order", orderRouter);
-app.use("/api/slider", sliderRouter);
 
 // --- YE HAI WO NAYA CODE (Isay yahan add karo) ---
 app.get("/api/health", (req, res) => {
