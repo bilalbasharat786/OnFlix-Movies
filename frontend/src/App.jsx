ddcca186a80690a948bb3634aa5ee3f54d736675
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-22 12:47:50 +0500

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Player from './pages/Player';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <Router>
       <div className="bg-black min-h-screen text-white">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/watch/:id" element={<Player />} />
