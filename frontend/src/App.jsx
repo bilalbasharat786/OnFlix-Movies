6926894fd248ae30392e091ad0a8cc05d13b1293
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-22 13:15:44 +0500

    app.jsx ma route define

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Player from './pages/Player';
 import Navbar from './components/Navbar';
+import Hollywood from './pages/Hollywood';
+import Bollywood from './pages/Bollywood';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/hollywood" element={<Hollywood />} />
+          <Route path="/bollywood" element={<Bollywood />} />
           <Route path="/watch/:id" element={<Player />} />
         </Routes>
       </div>
