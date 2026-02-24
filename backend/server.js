f6161e447f535dd95b034dbf6bc7aa52d55cd520
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-24 08:45:38 +0500

    app.get('/ping', (req, res) => res.send('ok'));

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ connectDB();
 // Routes
 app.use("/api/movies", movieRoutes);
 
+app.get('/ping', (req, res) => res.send('ok'));
+
 app.get("/", (req, res) => {
   res.send("Movie API is Running Successfully! 🚀");
 });
