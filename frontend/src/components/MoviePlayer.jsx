03c2c233b7602e40059bd9792ddcbb84f82cd49a
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-28 10:21:43 +0500

    350 or 7

--- frontend/src/components/MoviePlayer.jsx
+++ frontend/src/components/MoviePlayer.jsx
@@ -65,8 +65,8 @@ const MoviePlayer = () => {
           className="absolute border-none pointer-events-auto" 
           style={{
             width: '135%',       // Video ko horizontally zoom karega
-            height: '300%',      // Buttons ko frame se bahar rakhne ke liye space
-            top: '-10%',         // Header ko upar se 100% kaat dega
+            height: '350%',      // Buttons ko frame se bahar rakhne ke liye space
+            top: '-7%',         // Header ko upar se 100% kaat dega
                     
           }}
           allowFullScreen
