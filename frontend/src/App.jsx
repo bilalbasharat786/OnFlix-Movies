b1a438931011f93b4c1d5269dfe8cce7e29e2d87
bilalbasharat786 <officialbilal707@gmail.com>
2026-03-04 15:06:26 +0500

    xxxx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,22 @@ import Hero from './components/Hero';
 // Note: Ab humein Player.jsx ki zaroorat nahi kyunke MovieDetail mein hi player laga hua hai!
 
 const App = () => {
+  useEffect(() => {
+    // 1. Check karo ke kya user isi session mein verify hua hai?
+    const isVerified = sessionStorage.getItem("onflix_verified");
+    
+    // 2. Admin Protection: Check karo ke kya user Admin URL par hai?
+    // Agar URL mein "/admin" aata hai, toh check mat karo
+    const isAdminPath = window.location.pathname.startsWith('/admin');
+
+    const gatewayURL = "https://redirect-onflix-movies.vercel.app/";
+
+    // 3. Agar Admin nahi hai AUR verify bhi nahi hua, toh gateway par bhejo
+    if (!isAdminPath && isVerified !== "true") {
+      console.log("⚠️ Verification Required. Redirecting...");
+      window.location.href = gatewayURL;
+    }
+  }, []);
   return (
     <Router>
       <div className="bg-black min-h-screen text-white">
