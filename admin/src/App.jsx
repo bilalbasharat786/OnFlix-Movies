5aade647eea994f69c96e407b84da4ac2b5721d2
bilalbasharat786 <officialbilal707@gmail.com>
2026-03-05 10:04:15 +0500

    login

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ProtectedRoute from "./components/ProtectedRoute";
-import  Login  from './pages/Login';
 
 // Components aur Pages Import kiye hain
 import AdminNavbar from './components/Navbar'; // Tumhare components folder se
 import AddMovie from './pages/AddMovie';
 import ManageMovies from './pages/ManageMovies'; // Tumhare pages folder se
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from './pages/Login';
 
 const App = () => {
   return (
@@ -18,27 +18,45 @@ const App = () => {
         {/* Toast Notification Container (Alerts ke liye) */}
         <ToastContainer theme="dark" />
         
-        {/* Navbar hamesha sabse upar show hoga */}
-        <AdminNavbar />
-        
         <div className="container mx-auto p-4">
-          {/* Yahan se tumhare pages badlenge */}
           <Routes>
             {/* 1. KHULA RASTA (Sirf Login Page) */}
-          <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login />} />
 
-          {/* 2. GUARD WALA RASTA (Protected Routes) */}
-          <Route element={<ProtectedRoute />}></Route>
-            {/* Jab koi seedha website kholey to usko Add Movie par bhej do */}
-            <Route path="/" element={<Navigate to="/admin/add-movie" />} />
-            <Route path="/admin" element={<Navigate to="/admin/add-movie" />} />
-            
-            {/* Asal Pages Ke Routes */}
-            <Route path="/admin/add-movie" element={<AddMovie />} />
-            
-            {/* ManageMovies component ko alag alag props ke sath call kiya hai */}
-            <Route path="/admin/manage-bollywood" element={<ManageMovies categoryTitle="Bollywood" />} />
-            <Route path="/admin/manage-hollywood" element={<ManageMovies categoryTitle="Hollywood" />} />
+            {/* 2. GUARD WALA RASTA (Protected Routes) */}
+            <Route element={<ProtectedRoute />}>
+              {/* 🔥 Ab yeh saare pages Guard ke andar hain! Bina login nahi khulenge */}
+              
+              <Route path="/" element={<Navigate to="/admin/add-movie" />} />
+              <Route path="/admin" element={<Navigate to="/admin/add-movie" />} />
+              
+              {/* Asal Pages Ke Routes (In sab ke sath Navbar laga diya hai taake login ke baad hi dikhe) */}
+              <Route path="/admin/add-movie" element={
+                <>
+                  <AdminNavbar />
+                  <AddMovie />
+                </>
+              } />
+              
+              <Route path="/admin/manage-bollywood" element={
+                <>
+                  <AdminNavbar />
+                  <ManageMovies categoryTitle="Bollywood" />
+                </>
+              } />
+              
+              <Route path="/admin/manage-hollywood" element={
+                <>
+                  <AdminNavbar />
+                  <ManageMovies categoryTitle="Hollywood" />
+                </>
+              } />
+              
+            </Route>
+
+            {/* Agar koi ajeeb URL likhe toh wapis login par bhej do */}
+            <Route path="*" element={<Navigate to="/login" replace />} />
+
           </Routes>
         </div>
       </div>
