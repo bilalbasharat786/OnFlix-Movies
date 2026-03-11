bc0d0b4a2c9eb2cbcae4a33fce6eba2c0cdc832c
bilalbasharat786 <officialbilal707@gmail.com>
2026-03-11 12:29:11 +0500

    1vh or 20

--- frontend/src/components/MoviePlayer.jsx
+++ frontend/src/components/MoviePlayer.jsx
@@ -123,10 +123,10 @@ const MoviePlayer = () => {
         {/* ⬛ BLACK OVERLAYS (KAALI PATTIYAN - Desktop Only) ⬛ */}
         
         {/* 1. Top Header Cover */}
-        <div className="hidden md:block absolute top-0 left-0 w-full h-[2vh] bg-black z-[80] pointer-events-auto"></div>
+        <div className="hidden md:block absolute top-0 left-0 w-full h-[1vh] bg-black z-[80] pointer-events-auto"></div>
         
         {/* 2. Right Sidebar Cover */}
-        <div className="hidden md:block absolute top-0 right-0 w-[22vw] h-full bg-black z-[80] pointer-events-auto"></div>
+        <div className="hidden md:block absolute top-0 right-0 w-[20vw] h-full bg-black z-[80] pointer-events-auto"></div>
         
         {/* 3. Bottom Text Cover */}
         <div className="hidden md:block absolute bottom-0 left-0 w-full h-[18vh] bg-black z-[80] pointer-events-auto"></div>
